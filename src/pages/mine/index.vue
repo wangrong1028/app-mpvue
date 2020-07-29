42989fffb2ee80e6386fcc7f3a28bd76632c68a5
weiweiweia <2469670630@qq.com>
2020-07-29 17:50:30 +0800

    底部我的导航配置

---
+++ src/pages/mine/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+
+  data () {
+    return {
+
+    }
+  },
+
+  created () {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
